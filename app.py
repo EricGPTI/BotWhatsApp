89d9f1aeb5a461318d2400a0275896f46069a2ad
Eric <eric.silva@nsprevention.com.br>
2020-01-18 01:19:00 -0300

    Não está subindo o webdriver

--- app.py
+++ app.py
@@ -5,16 +5,10 @@ import logging
 from asyncio import sleep
 from webwhatsapi import WhatsAPIDriver
 
-
 logging.basicConfig(level=logging.ERROR)
 
 driver = WhatsAPIDriver(loadstyles=True)
 
-
-# connection = MongoClient('192.168.10.165', 27017)
-# db = connection.whatsapp
-#
-# es = Elasticsearch([{'host': '192.168.10.165', 'port': 9200}])
 
 def connect_bot():
     """
@@ -94,75 +88,5 @@ def save_word(words):
         w.save(word)
 
 
-'''
-    #contact = unread_msg()
-    #if contact is None:
-    #    main()
-    #for data_message in contact.messages:
-    #    try:
-    #        messsage = data_message.content
-    #    except AttributeError as e:
-    #        if str(e) == "'NoneType' object has no attribute 'messages'":
-    #            continue
-    #        elif str(e) == "'MMSMessage' object has no attribute 'content'":
-    #            continue
-    #        elif str(e) == "'MediaMessage' object has no attribute 'content'":
-    #            continue
-    #        elif str(e) == "'Message' object has no attribute 'content'":
-    #            continue
-    #        main()
-
-def insert_db(doc_update):
-    new_date_db = doc_update['Date_Msg']
-    dates = date_convert.date_time_db(new_date_db)
-
-    date_now = dates[0]
-    date_msg_converted = dates[1]
-    doc_update.update(Date_Msg=date_msg_converted, Date_Update=date_now)
-    counter = 0
-    try:
-        db.updates.insert_one(doc_update)
-        return True
-    except(BaseException) as e:
-        if str(e).__contains__("E11000 duplicate key error collection"):
-            print(e)
-            pass
-        else:
-            with open('log.txt', 'a') as l:
-                l.write("Date: " + str(date_now) + "\n" + "Error: " + str(e) + "\n" + "Local: InsertDB" + "\n\n")
-                counter += 1
-                if counter == 100:
-                    l.write("Date: " + str(
-                        date_now) + "\n" + "uma atualização não foi salva por problemas com o banco.\n\n")
-                    pass
-                else:
-                    sleep(3)
-            return False
-
-
-def insert_index(doc_update):
-    new_date_index = doc_update['Date_Msg']
-    dates = date_convert.date_index(new_date_index)
-    date_now = dates[0]
-    date_msg_converted = dates[1]
-    doc_update.update(Date_Msg=date_msg_converted, Date_Update=date_now)
-    counter = 0
-    del doc_update['_id']
-    try:
-        res = es.index(index="whatsapp", doc_type="whatsapp", body=doc_update)
-        return True
-    except(BaseException) as e:
-        date_error = str(doc_update['Date_Update'])
-        with open('log.txt', 'a') as l:
-            l.write("Date: " + date_error + "\n" + "Error: " + str(e) + "\n" + "Local: Insert_Index" + "\n\n")
-            counter += 1
-            if counter == 100:
-                l.write("Date: " + date_error + '\n' + 'uma atualização não foi indexada pelo seguinte erro: ' + str(
-                    e) + '\n\n')
-                pass
-            else:
-                sleep(10)
-                insert_index(doc_update)
-        return False
-        main()
-'''
+if __name__ == '__main__':
+    connect_bot()
