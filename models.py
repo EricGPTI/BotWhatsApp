b778a09c3e39e856c7eec9f102a05fddc8f90087
Eric <eric.silva@nsprevention.com.br>
2020-03-11 17:20:01 -0300

    Criado classe para chat que executa o salvamento do nome dos chats.

--- models.py
+++ models.py
@@ -16,3 +16,20 @@ class Message:
         client = self.client()
         db = client.dbbot
         db.message.insert_one(self.message)
+
+
+class Chat:
+
+    def __init__(self, db_url, port, chat):
+        self.db_url = db_url
+        self.port = port
+        self.chat = chat
+
+    def client(self):
+        client = MongoClient(self.db_url, self.port)
+        return client
+
+    def update_chat(self):
+        _client = self.client()
+        db = _client.dbbot
+        db.chat.update(self.chat, upset=True)
